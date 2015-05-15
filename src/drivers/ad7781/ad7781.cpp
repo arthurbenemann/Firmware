8cd7712f8578f59ebbac9f37d615696100ec7d3f
Arthur Benemann <arthur.benemann@gmail.com>
2015-05-15 02:40:47 -0300

    ad7781: remove unused register defines

--- src/drivers/ad7781/ad7781.cpp
+++ src/drivers/ad7781/ad7781.cpp
@@ -77,105 +77,6 @@
 #endif
 static const int ERROR = -1;
 
-/* Orientation on board */
-#define SENSOR_BOARD_ROTATION_000_DEG	0
-#define SENSOR_BOARD_ROTATION_090_DEG	1
-#define SENSOR_BOARD_ROTATION_180_DEG	2
-#define SENSOR_BOARD_ROTATION_270_DEG	3
-
-/* SPI protocol address bits */
-#define DIR_READ				(1<<7)
-#define DIR_WRITE				(0<<7)
-#define ADDR_INCREMENT				(1<<6)
-
-/* register addresses */
-#define ADDR_WHO_AM_I			0x0F
-#define WHO_I_AM_H 				0xD7
-#define WHO_I_AM				0xD4
-#define WHO_I_AM_L3G4200D		0xD3	/* for L3G4200D */
-
-#define ADDR_CTRL_REG1			0x20
-#define REG1_RATE_LP_MASK			0xF0 /* Mask to guard partial register update */
-
-/* keep lowpass low to avoid noise issues */
-#define RATE_95HZ_LP_25HZ		((0<<7) | (0<<6) | (0<<5) | (1<<4))
-#define RATE_190HZ_LP_25HZ		((0<<7) | (1<<6) | (0<<5) | (1<<4))
-#define RATE_190HZ_LP_50HZ		((0<<7) | (1<<6) | (1<<5) | (0<<4))
-#define RATE_190HZ_LP_70HZ		((0<<7) | (1<<6) | (1<<5) | (1<<4))
-#define RATE_380HZ_LP_20HZ		((1<<7) | (0<<6) | (1<<5) | (0<<4))
-#define RATE_380HZ_LP_25HZ		((1<<7) | (0<<6) | (0<<5) | (1<<4))
-#define RATE_380HZ_LP_50HZ		((1<<7) | (0<<6) | (1<<5) | (0<<4))
-#define RATE_380HZ_LP_100HZ		((1<<7) | (0<<6) | (1<<5) | (1<<4))
-#define RATE_760HZ_LP_30HZ		((1<<7) | (1<<6) | (0<<5) | (0<<4))
-#define RATE_760HZ_LP_35HZ		((1<<7) | (1<<6) | (0<<5) | (1<<4))
-#define RATE_760HZ_LP_50HZ		((1<<7) | (1<<6) | (1<<5) | (0<<4))
-#define RATE_760HZ_LP_100HZ		((1<<7) | (1<<6) | (1<<5) | (1<<4))
-
-#define ADDR_CTRL_REG2			0x21
-#define ADDR_CTRL_REG3			0x22
-#define ADDR_CTRL_REG4			0x23
-#define REG4_RANGE_MASK				0x30 /* Mask to guard partial register update */
-#define RANGE_250DPS				(0<<4)
-#define RANGE_500DPS				(1<<4)
-#define RANGE_2000DPS				(3<<4)
-
-#define ADDR_CTRL_REG5			0x24
-#define ADDR_REFERENCE			0x25
-#define ADDR_OUT_TEMP			0x26
-#define ADDR_STATUS_REG			0x27
-#define ADDR_OUT_X_L			0x28
-#define ADDR_OUT_X_H			0x29
-#define ADDR_OUT_Y_L			0x2A
-#define ADDR_OUT_Y_H			0x2B
-#define ADDR_OUT_Z_L			0x2C
-#define ADDR_OUT_Z_H			0x2D
-#define ADDR_FIFO_CTRL_REG		0x2E
-#define ADDR_FIFO_SRC_REG		0x2F
-#define ADDR_INT1_CFG			0x30
-#define ADDR_INT1_SRC			0x31
-#define ADDR_INT1_TSH_XH		0x32
-#define ADDR_INT1_TSH_XL		0x33
-#define ADDR_INT1_TSH_YH		0x34
-#define ADDR_INT1_TSH_YL		0x35
-#define ADDR_INT1_TSH_ZH		0x36
-#define ADDR_INT1_TSH_ZL		0x37
-#define ADDR_INT1_DURATION		0x38
-#define ADDR_LOW_ODR			0x39
-
-
-/* Internal configuration values */
-#define REG1_POWER_NORMAL			(1<<3)
-#define REG1_Z_ENABLE				(1<<2)
-#define REG1_Y_ENABLE				(1<<1)
-#define REG1_X_ENABLE				(1<<0)
-
-#define REG4_BDU				(1<<7)
-#define REG4_BLE				(1<<6)
-//#define REG4_SPI_3WIRE			(1<<0)
-
-#define REG5_FIFO_ENABLE			(1<<6)
-#define REG5_REBOOT_MEMORY			(1<<7)
-
-#define STATUS_ZYXOR				(1<<7)
-#define STATUS_ZOR				(1<<6)
-#define STATUS_YOR				(1<<5)
-#define STATUS_XOR				(1<<4)
-#define STATUS_ZYXDA				(1<<3)
-#define STATUS_ZDA				(1<<2)
-#define STATUS_YDA				(1<<1)
-#define STATUS_XDA				(1<<0)
-
-#define FIFO_CTRL_BYPASS_MODE			(0<<5)
-#define FIFO_CTRL_FIFO_MODE			(1<<5)
-#define FIFO_CTRL_STREAM_MODE			(1<<6)
-#define FIFO_CTRL_STREAM_TO_FIFO_MODE		(3<<5)
-#define FIFO_CTRL_BYPASS_TO_STREAM_MODE		(1<<7)
-
-#define L3GD20_DEFAULT_RATE			760
-#define L3G4200D_DEFAULT_RATE			800
-#define L3GD20_DEFAULT_RANGE_DPS		2000
-#define L3GD20_DEFAULT_FILTER_FREQ		30
-#define L3GD20_TEMP_OFFSET_CELSIUS		40
 
 #ifdef PX4_SPI_BUS_EXT
 #define EXTERNAL_BUS PX4_SPI_BUS_EXT
@@ -183,9 +84,6 @@ static const int ERROR = -1;
 #define EXTERNAL_BUS 0
 #endif
 
-#ifndef SENSOR_BOARD_ROTATION_DEFAULT
-#define SENSOR_BOARD_ROTATION_DEFAULT		SENSOR_BOARD_ROTATION_270_DEG
-#endif
 extern "C" { __EXPORT int ad7781_main(int argc, char *argv[]); }
 
 class AD7781 : public device::SPI
